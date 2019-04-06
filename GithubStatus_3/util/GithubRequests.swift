8ca760e84d27809bd7f041cd8a822efb9b414eff
zjhch123 <416836508@qq.com>
2019-04-06 22:40:57 +0800

    feat: 终于更新了一版

--- GithubStatus_3/util/GithubRequests.swift
+++ GithubStatus_3/util/GithubRequests.swift
@@ -41,11 +41,16 @@ class GithubRequests {
     func request() {
         let username = Utils.getUser()
         let headers = [
-            "Host": "github.com",
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, sdch, br",
-            "Cookie": "tz=Asia/Shanghai"
+            "Host":"github.com",
+            "Connection":"keep-alive",
+            "Pragma":"no-cache",
+            "Cache-Control":"no-cache",
+            "Upgrade-Insecure-Requests":"1",
+            "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
+            "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+            "Accept-Encoding":"gzip, deflate, br",
+            "Accept-Language":"zh-CN,zh;q=0.9,zh-TW;q=0.8,en;q=0.7,ja;q=0.6,la;q=0.5",
+            "Cookie":"tz=Asia%2FShanghai"
         ]
         HTTP.GET("https://github.com/" + username, headers: headers) { response in
             if let err = response.error {
