4edd1da9bcb3c7a0c61ce780a0145a5b0f692433
zjhch123 <416836508@qq.com>
2019-04-07 16:50:36 +0800

    feat: 更新webview样式

---
+++ GithubStatus_3/WeakScriptMessageDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  WeakScriptMessageDelegate.swift
+//  GithubStatus_3
+//
+//  Created by 张佳皓 on 2019/4/7.
+//  Copyright © 2019 张佳皓. All rights reserved.
+//
+
+import Foundation
--- webview/src/App.js
+++ webview/src/App.js
@@ -3,6 +3,8 @@ import '@css/style.scss';
 import cheerio from 'cheerio'
 import event from '@js/eventemit'
 
+window.event = event
+
 if (process.env.NODE_ENV !== 'production') {
   require('./index.html')
   const data = require('@assets/testData.html')
@@ -11,70 +13,100 @@ if (process.env.NODE_ENV !== 'production') {
   }, 100)
 }
 
-function render({headerUrl, username, nickname, profileBio, lastUpdate, todayCommit, todayColor, dailyGraph}) {
-  document.querySelector('.J_userHeader').setAttribute('src', headerUrl)
-  document.querySelector('.J_username').innerText = username
-  document.querySelector('.J_nickname').innerText = nickname
-  document.querySelector('.J_profile_bio').innerText = profileBio
-  document.querySelector('.J_last_update').innerText = lastUpdate
-  document.querySelector('.J_today').innerText = todayCommit
-  document.querySelector('.J_today_color').style.background = todayColor
-  document.querySelector('.J_daily').innerHTML = dailyGraph
-}
-
-function getLastUpdate($) {
-  const rects = $('rect')
-
-  let lastUpdate = 'Not update yet'
-
-  for (let i = rects.length - 1; i >= 0; i--) {
-    const $rect = rects.eq(i)
-    if ($rect.attr('data-count') != 0) {
-      lastUpdate = $rect.attr('data-date')
-      break
+const App = (() => {
+  function getLastUpdate($) {
+    const rects = $('rect')
+  
+    let lastUpdate = 'Not update yet'
+  
+    for (let i = rects.length - 1; i >= 0; i--) {
+      const $rect = rects.eq(i)
+      if ($rect.attr('data-count') != 0) {
+        lastUpdate = $rect.attr('data-date')
+        break
+      }
     }
+  
+    return lastUpdate
   }
 
-  return lastUpdate
-}
-
-function getDailyGraph($) {
-  const graphSvg = $('.js-calendar-graph-svg > g').find('g')
-  let tpl = ``
-  for (let i = 0; i < graphSvg.length; i++) {
-    const $c = graphSvg.eq(i)
-    const $rects = $c.find('rect')
-    let _tpl = ``
-    for (let j = 0; j < $rects.length; j++) {
-      const $rect = $rects.eq(j)
-      const count = $rect.attr('data-count')
-      _tpl += `<div class="rect" ${count > 0 ? 'data-hover="1"' : ''} style="background-color: ${$rect.attr('fill')}"></div>`
+  function getDailyGraph($) {
+    const graphSvg = $('.js-calendar-graph-svg > g').find('g')
+    let tpl = ``
+    for (let i = 0; i < graphSvg.length; i++) {
+      const $c = graphSvg.eq(i)
+      const $rects = $c.find('rect')
+      let _tpl = ``
+      for (let j = 0; j < $rects.length; j++) {
+        const $rect = $rects.eq(j)
+        const count = $rect.attr('data-count')
+        _tpl += `<div class="rect" ${count > 0 ? 'data-hover="1"' : ''} style="background-color: ${$rect.attr('fill')}"></div>`
+      }
+      tpl += `<div class="group">${_tpl}</div>`
     }
-    tpl += `<div class="group">${_tpl}</div>`
+    return tpl
   }
-  return tpl
-}
 
-event.on('getData', (data) => {
-  const $ = cheerio.load(data)
+  function renderDOM({headerUrl, username, nickname, profileBio, lastUpdate, todayCommit, todayColor, dailyGraph}) {
+    document.querySelector('.J_userHeader').setAttribute('src', headerUrl)
+    document.querySelector('.J_username').innerText = username
+    document.querySelector('.J_nickname').innerText = nickname
+    document.querySelector('.J_profile_bio').innerText = profileBio
+    document.querySelector('.J_last_update').innerText = lastUpdate
+    document.querySelector('.J_today').innerText = todayCommit
+    document.querySelector('.J_today_color').style.background = todayColor
+    document.querySelector('.J_daily').innerHTML = dailyGraph
+  }
 
-  const headerUrl = $('.u-photo .avatar').attr('src')
-  const username = $('.p-name').html()
-  const nickname = $('.p-nickname').html()
+  function render(data) {
+    const $ = cheerio.load(data)
 
-  const profileBio = $('.js-user-profile-bio').text()
+    const headerUrl = $('.u-photo .avatar').attr('src')
+    const username = $('.p-name').html()
+    const nickname = $('.p-nickname').html()
 
-  const today = $('rect').last()
-  const todayCommit = today.attr('data-count')
-  const todayColor = today.attr('fill')
+    const profileBio = $('.js-user-profile-bio').text()
 
-  const dailyGraph = getDailyGraph($)
+    const today = $('rect').last()
+    const todayCommit = today.attr('data-count')
+    const todayColor = today.attr('fill')
 
-  const lastUpdate = getLastUpdate($)
+    const dailyGraph = getDailyGraph($)
 
-  render({ headerUrl, username, nickname, profileBio, lastUpdate, todayCommit, todayColor, dailyGraph })
-})
+    const lastUpdate = getLastUpdate($)
 
-document.body.addEventListener('contextmenu', (e) => {
-  e.preventDefault()
-})
+    renderDOM({ headerUrl, username, nickname, profileBio, lastUpdate, todayCommit, todayColor, dailyGraph })
+  }
+
+  function listen() {
+    document.body.addEventListener('contextmenu', (e) => {
+      e.preventDefault()
+    })
+
+    document.querySelector('.J_setting').addEventListener('submit', (e) => {
+      e.preventDefault()
+
+      const username = document.querySelector('.J_username_input').value
+      try {
+        window.webkit.messageHandlers.setUser.postMessage(username)
+      } catch (_) { }
+      document.body.classList.remove('f-setting')
+    })
+
+    event.on('getData', (data) => {
+      App.render(data)
+    })
+
+    event.on('setting', (_) => {
+      document.body.classList.add('f-setting')
+      document.querySelector('.J_username_input').value = document.querySelector('.J_nickname').innerText
+    })
+  }
+
+  return {
+    render,
+    listen
+  }
+})()
+
+App.listen()
